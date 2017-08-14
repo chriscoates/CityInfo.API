57c606270a20e8fdab2f87e23c039db7526465a8
Chris Coates <coates.c@hotmail.com>
2017-08-14 20:29:35 +0100

    correct typo

--- src/CityInfo.API/Services/LocalMailService.cs
+++ src/CityInfo.API/Services/LocalMailService.cs
@@ -4,9 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CityInfo.API.Services
+namespace CityInfo.API.Service
 {
-  public class LocalMailServices
+  public class LocalMailService
   {
     private string _mailTo = "admin@mycompany.com";
     private string _mailFrom = "noreply@mycompany.com";
