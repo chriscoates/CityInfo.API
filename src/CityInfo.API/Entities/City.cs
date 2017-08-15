51bdffb9049bf991d062ff84b8d14532c4995337
Chris Coates <coates.c@hotmail.com>
2017-08-15 20:22:49 +0100

    added entities

---
+++ src/CityInfo.API/Entities/City.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+  public class City
+  {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+
+    [MaxLength(50)]
+    public int Description { get; set; }
+    public ICollection<PointOfInterest> PointsOfInterest { get; set; }
+    = new List<PointOfInterest>();
+
+  }
+}
