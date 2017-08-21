d57c0d931bb0485b41315a0c29a174e17383cd41
Chris Coates <coates.c@hotmail.com>
2017-08-21 15:59:24 +0100

    modification for .net core 2.0

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace CityInfo.API
@@ -7,14 +8,12 @@ namespace CityInfo.API
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        private static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-            host.Run();
-        }
     }
 }
