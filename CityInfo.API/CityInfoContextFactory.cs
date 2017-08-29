3459db341dce242efdf3b9bfab9cbe0751f8bcfc
Chris Coates <coates.c@hotmail.com>
2017-08-29 13:21:00 +0100

    adding context factory

---
+++ CityInfo.API/CityInfoContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CityInfo.API
+{
+    public class CityInfoContextFactory : IDesignTimeDbContextFactory<CityInfoContext>
+    {
+        public CityInfoContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Server=(localdb)\\ProjectsV13;Database=CityInfoDB;Trusted_Connection=True;";
+
+            var optionsBuilder = new DbContextOptionsBuilder<CityInfoContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new CityInfoContext(optionsBuilder.Options);
+        }
+    }
+}
