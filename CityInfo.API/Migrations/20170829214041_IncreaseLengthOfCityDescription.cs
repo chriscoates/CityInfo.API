59c1dda41eb92f0e5972e6a16d10ae1abd47b7dd
Chris Coates <coates.c@hotmail.com>
2017-08-29 22:57:47 +0100

    adding migration

---
+++ CityInfo.API/Migrations/20170829214041_IncreaseLengthOfCityDescription.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CityInfo.API.Migrations
+{
+    public partial class IncreaseLengthOfCityDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Cities",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Cities",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+    }
+}
