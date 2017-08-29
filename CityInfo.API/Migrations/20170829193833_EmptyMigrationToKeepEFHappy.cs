6face50b76c63bcce969d365fbb734fd8b62eb6b
Chris Coates <coates.c@hotmail.com>
2017-08-29 22:21:15 +0100

    manually adding migration

---
+++ CityInfo.API/Migrations/20170829193833_EmptyMigrationToKeepEFHappy.cs
@@ -0,0 +1,37 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CityInfo.API.Migrations
+{
+    public partial class EmptyMigrationToKeepEFHappy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+               name: nameof(City.Description),
+               table: "Cities");
+
+            migrationBuilder.AddColumn<string>(
+                name: nameof(City.Description),
+                table: "Cities",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+               name: nameof(City.Description),
+               table: "Cities");
+
+            migrationBuilder.AddColumn<int>(
+                name: nameof(City.Description),
+                table: "Cities",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
