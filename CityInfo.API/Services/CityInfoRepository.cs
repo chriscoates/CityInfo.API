602b37f0d9cbbf7905b61e6992bd0b6c80cc68b7
Chris Coates <coates.c@hotmail.com>
2017-08-31 13:03:25 +0100

    adding CityInfoImplementation

---
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private CityInfoContext _context;
+        public CityInfoRepository(CityInfoContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<City> GetCities()
+        {
+            return _context.Cities.OrderBy(c => c.Name).ToList();
+        }
+
+        public City GetCity(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PointOfInterest GetPointOFInterestForCity(int cityId, int pointOfInterestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PointOfInterest> GetPointsOfInterestForCity(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
