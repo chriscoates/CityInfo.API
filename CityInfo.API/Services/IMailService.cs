60a793dec16fd74c5f032d5f2fb4901387444731
Chris Coates <coates.c@hotmail.com>
2017-09-19 16:10:22 +0100

    update IMailService contract

--- CityInfo.API/Services/IMailService.cs
+++ CityInfo.API/Services/IMailService.cs
@@ -1,7 +1,11 @@
+using System.Threading.Tasks;
+
 namespace CityInfo.API.Services
 {
     public interface IMailService
-  {
-    void Send(string subject, string message);
-  }
+    {
+        Task SendAsync(string subject, string message);
+        Task SendAsync(string subject, string message, string toEmail, string toName);
+        Task SendAsync(string subject, string message, string fromEmail, string fromName, string toEmail, string toName);
+    }
 }
